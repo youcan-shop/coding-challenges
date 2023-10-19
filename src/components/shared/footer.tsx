1334b4877369ddf555cd47fe7331bd945272da35
PACY2 <>
2023-10-19 00:40:17 +0000

    feat:Add public layout

---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="h-16 border-t justify-center flex items-center">
+      © Copyright YouCan Challenge 2023
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} className={inter.className} />;
 }
